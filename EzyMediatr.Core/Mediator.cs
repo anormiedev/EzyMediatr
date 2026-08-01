5e697d7ac0850957ba27bf751f2d28925d68f0ac
Mister Bista <thukpaa@gmail.com>
2026-08-01 16:42:43 +0545

    perf: reduce mediator dispatch overhead

--- EzyMediatr.Core/Mediator.cs
+++ EzyMediatr.Core/Mediator.cs
@@ -7,38 +7,34 @@ using EzyMediatr.Core.Internal;
 
 namespace EzyMediatr.Core;
 
-public class Mediator(IServiceProvider serviceProvider) : IMediator
+public class Mediator(IServiceProvider serviceProvider, EzyMediatrOptions options) : IMediator
 {
+    public Mediator(IServiceProvider serviceProvider)
+        : this(serviceProvider, serviceProvider.GetService<EzyMediatrOptions>() ?? new EzyMediatrOptions())
+    {
+    }
 
     public async Task<TResponse> Send<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default)
     {
         ArgumentNullException.ThrowIfNull(request);
 
-        using var scope = serviceProvider.CreateScope();
-        var wrapper = RequestHandlerWrapperFactory.Create(request.GetType(), typeof(TResponse));
-        var result = await wrapper.Handle(request, scope.ServiceProvider, cancellationToken).ConfigureAwait(false);
-        return (TResponse)result!;
+        var wrapper = RequestHandlerWrapperFactory<TResponse>.Create(request.GetType());
+        return await wrapper.Handle(request, serviceProvider, options, cancellationToken).ConfigureAwait(false);
     }
 
-    public async IAsyncEnumerable<TResponse> Stream<TResponse>(IStreamRequest<TResponse> request, [EnumeratorCancellation] CancellationToken cancellationToken = default)
+    public IAsyncEnumerable<TResponse> Stream<TResponse>(IStreamRequest<TResponse> request, CancellationToken cancellationToken = default)
     {
         ArgumentNullException.ThrowIfNull(request);
 
-        using var scope = serviceProvider.CreateScope();
-        var wrapper = StreamRequestHandlerWrapperFactory.Create(request.GetType(), typeof(TResponse));
-
-        await foreach (var item in wrapper.Handle(request, scope.ServiceProvider, cancellationToken).WithCancellation(cancellationToken))
-        {
-            yield return (TResponse)item!;
-        }
+        var wrapper = StreamRequestHandlerWrapperFactory<TResponse>.Create(request.GetType());
+        return wrapper.Handle(request, serviceProvider, cancellationToken);
     }
 
     public async Task Publish<TNotification>(TNotification notification, CancellationToken cancellationToken = default) where TNotification : INotification
     {
         ArgumentNullException.ThrowIfNull(notification);
 
-        using var scope = serviceProvider.CreateScope();
         var wrapper = NotificationHandlerWrapperFactory.Create(notification.GetType());
-        await wrapper.Handle(notification, scope.ServiceProvider, cancellationToken);
+        await wrapper.Handle(notification, serviceProvider, cancellationToken);
     }
 }
