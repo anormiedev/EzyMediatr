6808283c2cbcfbd3f9e102ee91e6f428f3f6867d
Mister Bista <thukpaa@gmail.com>
2026-08-01 16:42:42 +0545

    perf: streamline notification dispatch

--- EzyMediatr.Core/Internal/NotificationHandlerWrapper.cs
+++ EzyMediatr.Core/Internal/NotificationHandlerWrapper.cs
@@ -2,6 +2,7 @@ using System.Collections.Concurrent;
 using Microsoft.Extensions.DependencyInjection;
 using EzyMediatr.Core.Abstractions;
 using EzyMediatr.Core.Handlers;
+using EzyMediatr.Core.Pipeline;
 
 namespace EzyMediatr.Core.Internal;
 
@@ -13,15 +14,51 @@ internal abstract class NotificationHandlerWrapper
 internal sealed class NotificationHandlerWrapper<TNotification> : NotificationHandlerWrapper
     where TNotification : INotification
 {
-    public override async Task Handle(INotification notification, IServiceProvider serviceProvider, CancellationToken cancellationToken)
+    public override Task Handle(INotification notification, IServiceProvider serviceProvider, CancellationToken cancellationToken)
     {
         var typedNotification = (TNotification)notification;
-        var handlers = serviceProvider.GetServices<INotificationHandler<TNotification>>().ToList();
 
-        foreach (var handler in handlers)
+        Task ExecuteHandlers()
         {
-            await handler.Handle(typedNotification, cancellationToken);
+            var registeredHandlers = serviceProvider.GetServices<INotificationHandler<TNotification>>();
+            var handlers = registeredHandlers as INotificationHandler<TNotification>[]
+                ?? registeredHandlers.ToArray();
+
+            return handlers.Length switch
+            {
+                0 => Task.CompletedTask,
+                1 => handlers[0].Handle(typedNotification, cancellationToken),
+                _ => ExecuteSequentially(handlers)
+            };
         }
+
+        async Task ExecuteSequentially(INotificationHandler<TNotification>[] handlers)
+        {
+            foreach (var handler in handlers)
+            {
+                await handler.Handle(typedNotification, cancellationToken).ConfigureAwait(false);
+            }
+        }
+
+        var registeredBehaviors = serviceProvider.GetServices<INotificationPipelineBehavior<TNotification>>();
+        var behaviors = registeredBehaviors as INotificationPipelineBehavior<TNotification>[]
+            ?? registeredBehaviors.ToArray();
+
+        if (behaviors.Length == 0)
+        {
+            return ExecuteHandlers();
+        }
+
+        NotificationHandlerDelegate handlerDelegate = ExecuteHandlers;
+
+        for (var index = behaviors.Length - 1; index >= 0; index--)
+        {
+            var behavior = behaviors[index];
+            var next = handlerDelegate;
+            handlerDelegate = () => behavior.Handle(typedNotification, next, cancellationToken);
+        }
+
+        return handlerDelegate();
     }
 }
 
